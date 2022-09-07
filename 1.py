8d543c262f926de44d3be056bfcf5d21acc52b0e
Jennika Wilson <j.r.a.112100@gmail.com>
2022-09-07 19:28:45 -0400

    Done

--- 1.py
+++ 1.py
@@ -1,1 +1,5 @@
 # Write your solution for algorithm 1 below
+lst = [7, 9, 2, 6]
+lst.sort()
+
+print(lst)
--- 2.py
+++ 2.py
@@ -1,1 +1,6 @@
 # Write your solution for algorithm 2 below
+uns_lst = [7, 5, 9, 2]
+
+sortd_lst = sorted(uns_lst)
+
+print(sortd_lst)
